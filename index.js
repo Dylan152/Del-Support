473584d2f9ee12ccd9f1738f13ddc6f66218faea
Dylan152 <48649553+Dylan152@users.noreply.github.com>
2019-03-17 15:26:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const Discord = require("discord.js")
 
 const client = new Discord.Client();
-client.login("NTQwMjI5NTY0ODExNDQ0MjQ0.DzTPXQ.j-hu4kZSthqhzsuOCh8Gba3ZniM");
+client.login('process.env.TOKEN');
 
 
 const sql = require("sqlite");
